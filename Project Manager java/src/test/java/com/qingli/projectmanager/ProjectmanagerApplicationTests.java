e33566dbeb5c795b8ab732d4d6210e50cc7ac2c7
Qingli Zhang <qz2376@columbia.edu>
2020-06-11 11:42:40 -0400

    first commit

---
+++ Project Manager java/src/test/java/com/qingli/projectmanager/ProjectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingli.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
